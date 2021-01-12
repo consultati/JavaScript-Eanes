483c392020fe47fbefcde9d6a60dfec884f86e98
consultati <eazurara@gmail.com>
2021-01-11 23:20:26 -0300

    create local Storage
    Using Local Storage to persist data

--- exemplos/CRUD LocalStorage/app.js
+++ exemplos/CRUD LocalStorage/app.js
@@ -13,10 +13,24 @@ const app = new Vue({
         estado: false
       });
       this.novaTarefa = ''
+      localStorage.setItem('gym-vue', JSON.stringify(this.tarefas));
     },
     editarTarefa: function(index){
       //console.log('editar ', index)
-      this.tarefas[index].estado = true
+      this.tarefas[index].estado = true;
+      localStorage.setItem('gym-vue', JSON.stringify(this.tarefas));
+    },
+    eliminarTarefa: function(index){
+      this.tarefas.splice(index, 1);
+      localStorage.setItem('gym-vue', JSON.stringify(this.tarefas));
+    }
+  },
+  created: function(){
+    let dadosDB = JSON.parse(localStorage.getItem('gym-vue'))
+    if(dadosDB === null){
+      this.tarefas = [];
+    }else{
+      this.tarefas = dadosDB;
     }
   }
 });
