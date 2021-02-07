f024a57f368eb30095c3c12ecf401188687018aa
consultati <eazurara@gmail.com>
2021-02-07 10:39:52 -0300

    Funções com Date()
    Testando funções com Date()

--- exemplos/funções/ex01.js
+++ exemplos/funções/ex01.js
@@ -38,3 +38,59 @@ const sumarB = (num1 = 10) => (
 )
 let resultadoA = sumarB()
 console.log(resultadoA)
+
+var uid = (new Date().getTime()).toString(36)
+console.log('O numero gerado é:', uid);
+
+var date = new Date();
+var components = [
+    date.getYear(),
+    date.getMonth(),
+    date.getDate(),
+    date.getHours(),
+    date.getMinutes(),
+    date.getSeconds(),
+    date.getMilliseconds()
+];
+
+var id = components.join("");
+console.log('A data é: ', date);
+console.log('O numero gerado é:', id);
+
+var agora = Date.now();
+console.log('Valor :', agora);
+
+var date = new Date();
+let ano = date.getFullYear().toString()
+console.log('Valor Ano :', ano);
+
+let mes = (date.getMonth() + 1).toString()
+mes = pad(mes,2);
+console.log('Valor Mes :', mes);
+
+let dia = date.getDate().toString()
+dia = pad(dia,2);
+console.log('Valor Dia :', dia);
+
+let hora = date.getHours().toString()
+hora = pad(hora,2);
+console.log('Valor Hora :', hora);
+
+let min = date.getMinutes().toString()
+min = pad(min,2);
+console.log('Valor Minuto :', min);
+
+let sec = date.getSeconds().toString()
+sec = pad(sec,2);
+console.log('Valor Segundo :', sec);
+
+let seq = date.getMilliseconds().toString()
+console.log('Valor Seq :', seq);
+
+let ficha = ano + mes + dia + hora + min + sec
+console.log('Ficha: ', ficha);
+
+function pad(str, length) {
+  const resto = length - String(str).length;
+  return '0'.repeat(resto > 0 ? resto : '0') + str;
+}
